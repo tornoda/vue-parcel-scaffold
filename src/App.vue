49dd1702c27817cade7fd7395e4c59f95afa1e46
looyulong <looyulong@126.com>
2019-06-08 14:14:09 +0800

    vue组件打通

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    hello world
+    <HelloWorld></HelloWorld>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import HelloWorld from "./components/Helloworld.vue";
+export default Vue.extend({
+  components: {
+    HelloWorld
+  }
+});
+</script>
+
---
+++ src/components/Helloworld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="tips" @click="handleClick">{{msg}}</div>
+</template>
+
+<script>
+import Vue from "vue";
+export default Vue.extend({
+  name: "HelloWorld",
+  data: () => ({ msg: "hello, looyulong" }),
+  methods: {
+      handleClick() {
+
+alert(this.msg)
+      }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.tips {
+  color: red;
+}
+</style>
