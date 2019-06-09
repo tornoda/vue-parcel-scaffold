90bfd5618cb644480bf2feaf2662fcfd5d9079dc
looyulong <looyulong@126.com>
2019-06-09 23:08:27 +0800

    add prettierrc then test it

---
+++ prettierrc.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 4,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: "as-needed",
+  trailingComma: "es5",
+  bracketSpacing: true,
+  arrowParens: "avoid"
+};
